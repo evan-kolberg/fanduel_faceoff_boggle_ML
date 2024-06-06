e246360a2ba487a6564b80f7602298144e45b9e7
evankolberg <evankolberg@aol.com>
2024-06-06 16:09:00 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from paddleocr import PaddleOCR, draw_ocr
+
+# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+img_path = 'test.png'
+result = ocr.ocr(img_path, cls=True)
+for idx in range(len(result)):
+    res = result[idx]
+    for line in res:
+        print(line)
+
+# 显示结果
+from PIL import Image
+result = result[0]
+image = Image.open(img_path).convert('RGB')
+boxes = [line[0] for line in result]
+txts = [line[1][0] for line in result]
+scores = [line[1][1] for line in result]
+im_show = draw_ocr(image, boxes, txts, scores, font_path='simfang.ttf')
+im_show = Image.fromarray(im_show)
+im_show.save('result.jpg')
+
+print(txts)
+
+processed_list = ['I' if char == '一' else char for char in txts if not char.isdigit()]
+
+output = '\n'.join([''.join(processed_list[i:i+4]) for i in range(0, len(processed_list), 4)])
+
+print(output)
+
+with open('output.txt', 'w') as file:
+    file.write(output)
+
+
+
+
+
+
+
