f0a100244ba40bb082030a283ff9b93cde757e5f
Evan Kolberg <evankolberg@aol.com>
2024-06-11 22:33:42 -0400

    wip

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from pyggle.lib.pyggle import Boggle
-import io
-import sys
+import time
+from pyggle.lib.pyggle import Boggle, boggle
 
-# Separate columns by spaces
-#
 #  (0,0)  - x +
 #  -   Y A E L 
 #  y   N M K Y
@@ -12,15 +9,22 @@ import sys
 #           (3,3)
 
 # INPUT BOARD
-board_str = "YNOO AMKO EKEJ LYSI"
+board = [['Y', 'A', 'E', 'L'],
+         ['N', 'M', 'R', 'Y'],
+         ['O', 'K', 'E', 'S'],
+         ['O', 'O', 'J', 'I']
+]
+
+boggle = Boggle(board)
 
 # INPUT BONUS TILES
 bonus_tiles = {
-    'TL': [(1, 1), (3, 0), (0, 3), (3, 2)], 
-    'DL': [],         
-    'TW': [(2, 2)],         
-    'DW': []  
+    'TL': [(1, 1), (3, 0), (0, 3), (3, 2)],
+    'DL': [],
+    'TW': [(0, 3)],
+    'DW': []
 }
+
 
 # SCORING FROM https://www.fanduel.com/faceoff-training-guide-boggle
 
@@ -30,34 +34,13 @@ letter_points = {
     'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10
 }
 
-board = [list(row) for row in board_str.split()]
-
-boggle = Boggle(board_str)
-
-captured_output = io.StringIO()
-sys.stdout = captured_output
-boggle.print_result()
-captured_output.seek(0)
-
-output = captured_output.getvalue()
-
-sys.stdout = sys.__stdout__
-
-words_with_coords = {}
-for line in output.splitlines():
-    if line.strip(): 
-        word, coords_str = line.split(':')
-        word = word.strip()
-        coords = eval(coords_str.strip())
-        words_with_coords[word] = coords
-
-def calculate_word_score(word, coords, bonus_tiles, letter_points):
+def calculate_word_score(word, coords, board, bonus_tiles, letter_points) -> int:
     word_score = 0
     word_multipliers = []
     
     for (x, y) in coords:
         letter = board[x][y]
-        base_score = letter_points[letter.upper()]
+        base_score = letter_points[letter]
         
         for bonus, positions in bonus_tiles.items():
             if (x, y) in positions:
@@ -65,15 +48,12 @@ def calculate_word_score(word, coords, bonus_tiles, letter_points):
                     base_score *= 3
                 elif bonus == 'DL':
                     base_score *= 2
-        
-        word_score += base_score
-        
-        for bonus, positions in bonus_tiles.items():
-            if (x, y) in positions:
-                if bonus == 'TW':
+                elif bonus == 'TW':
                     word_multipliers.append(3)
                 elif bonus == 'DW':
                     word_multipliers.append(2)
+        
+        word_score += base_score
     
     for multiplier in word_multipliers:
         word_score *= multiplier
@@ -81,18 +61,28 @@ def calculate_word_score(word, coords, bonus_tiles, letter_points):
     if len(word) >= 5:
         bonus_points = (len(word) - 4) * 5
         word_score += bonus_points
-    
+
     return word_score
 
+
+solved = boggle.solve()
+time_taken = boggle.time_solve()
+print(solved)
+print(f"Compute Time: {time_taken}")
+
 word_scores = []
-for word, coords in words_with_coords.items():
-    score = calculate_word_score(word, coords, bonus_tiles, letter_points)
+for word, coords in solved.items():
+    score = calculate_word_score(word, coords, board, bonus_tiles, letter_points)
     word_scores.append((word, score))
 
 word_scores.sort(key=lambda x: x[1], reverse=True)
 
 for word, score in word_scores:
     print(f"{word}: {score}")
+
+
+
+
 
 
 
